61fc80fcb03b65ce9754d9b9267dcbe0763630ca
merrebach <michael.medenbach@proton.me>
2024-10-06 17:07:27 +0200

    finished guessing game

--- 2 Guessing Game/src/main.rs
+++ 2 Guessing Game/src/main.rs
@@ -1,25 +1,47 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
-    let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number :{secret_number}.");
+    // println!("The secret number: {secret_number}.");
 
-    println!("Please input your guess.");
+    // Infinite loop
+    loop {
+        println!("Please input your guess.");
 
-    // Random number
-    // mut makes a variable mutable
-    // By default, variables are immutable
-    let mut guess: String = String::new();
+        // Random number
+        // mut makes a variable mutable
+        // By default, variables are immutable
+        let mut guess: String = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line...");
+        // Reding the userinput to the mutable guess variable
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line...");
 
-    println!("You guessed: {}", guess);
+        // Shadowing lets us reuse the guess variable name rather than forcing us to create two unique variables.
+        // parse() returns an Ok or Err value, that we are matching and handling the user made error if f.e. no number is given.
+        let guess: u32 = match guess.trim().parse::<u32>() {
+            Ok(num) => num,
+            // The _ ist a "catchall" that catches every error type
+            Err(_) => continue,
+        };
 
-    // println!("other alternative to print the guess is so: {guess}.");
+        println!("You guessed: {}", guess);
+        // println!("other alternative to print the guess is so: {guess}.");
+
+        // Basically a switch case
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("You guessed to low."),
+            Ordering::Greater => println!("You guessed to high."),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
